4283e8172c22b75785cfcd9ea9495436662c0d4e
rabiya faizal <rabiyaf1@gmail.com>
2020-06-14 02:59:23 +0530

    implemented basic view purchase history

--- src/components/profile/PurchaseHistory.js
+++ src/components/profile/PurchaseHistory.js
@@ -2,8 +2,40 @@ import React, { Component } from 'react';
 import SingleOrder from './SingleOrder';
 import Navbar from '../layout/Navbar';
 import Footer from '../layout/Footer';
+import Moment from 'react-moment';
+const axios = require("axios");
 
 class PurchaseHistory extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            orders:[]
+        }
+    }
+
+    componentDidMount(){
+        const that = this;
+
+        const token = 'Bearer '+ localStorage.token;
+        const headersInfo = {
+            Authorization:token
+        }
+        const data = {
+            email:localStorage.email
+        }
+        
+        axios.post("http://localhost:8080/GetOrderListByUser",data,{
+            headers:headersInfo
+        }).then(function(res){
+            that.setState({
+                orders:res.data
+            })
+            console.log(res.data);
+        }).catch(function(error){
+            console.log(error);
+        })
+    }
+
     render() {
         return (
             <div>
@@ -15,16 +47,23 @@ class PurchaseHistory extends Component {
                             <tr>
                                 <th><h1>Order ID</h1></th>
                                 <th><h1>Date</h1></th>
-                                <th><h1>No. of Items</h1></th>
+                                <th><h1>Status</h1></th>
                                 <th><h1>Total</h1></th>
                             </tr>
                         </thead>
                         <tbody>
-                            <SingleOrder/>
-                            <SingleOrder/>
-                            <SingleOrder/>
-                            <SingleOrder/>
-                            <SingleOrder/>
+                            { this.state.orders && this.state.orders.map(order => {
+                                if(order.status!=="Pending"){
+                                    return(
+                                    <tr>
+                                        <td>{order.id}</td>
+                                        <td><Moment format="YYYY/MM/DD">{order.purchasedDate}</Moment></td>
+                                        <td>{order.status}</td>
+                                        <td>{"US$"+order.totalAmount}</td>
+                                    </tr>
+                                ) 
+                                }
+                            })}
                         </tbody>
                     </table>
                 </div>
--- src/components/profile/SingleOrder.js
+++ src/components/profile/SingleOrder.js
@@ -1,12 +1,13 @@
 import React from 'react'
 
-const SingleOrder = () => {
+const SingleOrder = ({order}) => {
     return (
         <tr>
-            <td>00453</td>
-            <td>12/09/20</td>
-            <td>5</td>
-            <td>Rs.7400.00</td>
+            <td>{order.id}</td>
+            <td>{order.purchasedDate}</td>
+            <td>{order.status}</td>
+            <td>{order.orderedBooks}</td>
+            <td>{order.totalAmount}</td>
         </tr>
     )
 }
